10d53f66af6ac38a783c1ccaf7b4f02cad20c59d
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-02-07 00:49:39 -0600

    Update Cajas.py

--- Cajas.py
+++ Cajas.py
@@ -13,15 +13,17 @@ size = (800,500)
 #Crear ventana
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
+pygame.mouse.set_visible(0)
 
 while True:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill(White)
     mouse = pygame.mouse.get_pos()
     x = mouse[0]
     y = mouse[1]
-    pygame.draw.rect(screen,Blue,(x-50,y-50,100,100))
+    pygame.draw.rect(screen,Blue,(x,y,20,20))
     pygame.display.flip()
     clock.tick(60)
