566b652194d7786fdca3bc1808bf8dbeb1b1905a
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-02-06 23:02:14 -0600

    Update game.py

--- game.py
+++ game.py
@@ -8,5 +8,5 @@ screen = pygame.display.set_mode(size)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit
+        if event.type == pygame.quit:
+            sys.exit()
